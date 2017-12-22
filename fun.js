1d83753497da0ef59a332ed171ed0903db9502e9
Chandra mouli <codechandra@gmail.com>
2017-12-22 08:01:38 +0530

    initial commit

---
+++ fun.js
@@ -0,0 +1,22 @@
+function a()
+{
+  document.getElementById('bd').style.display="none";
+  document.getElementById('fsd').style.display="none";
+  document.getElementById('fd').style.display="block";
+}
+function b()
+{
+  document.getElementById('bd').style.display="block";
+  document.getElementById('fsd').style.display="none";
+  document.getElementById('fd').style.display="none";
+}
+function c()
+{
+  document.getElementById('bd').style.display="none";
+  document.getElementById('fsd').style.display="block";
+  document.getElementById('fd').style.display="none";
+}
+function f()
+{
+  document.getElementById('button').style.display="block";
+}
